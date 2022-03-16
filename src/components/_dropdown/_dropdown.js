f6d5216a4dbd07c3f400d3548204b3cb1f114040
Nickolai Khitrov <deliriumbred@yandex.ru>
2022-03-16 20:57:33 +0300

    Add functional of dropdown footer buttons - clear items to default values and apply

--- src/components/_dropdown/_dropdown.js
+++ src/components/_dropdown/_dropdown.js
@@ -68,9 +68,42 @@ function toggleDropdown(dropdown) {
   dropdown.classList.toggle('dropdown_open');
 }
 
+function clearItemsDefault(items) {
+  items.forEach(item => {
+    const initialCount = item.dataset.minimum;
+    const minusBtn = item.querySelector('.js-dropdown__item-btn[data-btn-value="-"]');
+    const itemCount = item.querySelector('.js-dropdown__count-int');
+
+    itemCount.innerText = initialCount;
+    minusBtn.setAttribute('disabled', '');
+  })
+}
+
+function removeDisabledBtn(btn) {
+  btn.removeAttribute('disabled');
+}
+
+function checkChangeItemOnce(items) {
+  for (let item of items) {
+    const initialCount = item.dataset.minimum;
+    const nowCountValue = item.querySelector('.js-dropdown__count-int').innerText;
+
+    if (initialCount !== nowCountValue) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 function dropdownHandler(e) {
   const items = this.querySelectorAll('.js-dropdown__item');
   const dropBtn = this.querySelector('.js-dropdown__drop-btn');
+
+  const footerButtons = {
+    clear: this.querySelector('.js-dropdown__footer-btn_clear'),
+    apply: this.querySelector('.js-dropdown__footer-btn_apply'),
+  }
 
   if (e.target === dropBtn) {
     toggleDropdown(e.currentTarget);
@@ -80,6 +113,20 @@ function dropdownHandler(e) {
 
   if (e.target.classList.contains('js-dropdown__item-btn')) {
     countItems(e.currentTarget);
+  }
+
+  if (e.target == footerButtons.clear) {
+    clearItemsDefault(items);
+    countItems(e.currentTarget);
+    footerButtons.clear.setAttribute('disabled', '');
+  }
+
+  if (e.target == footerButtons.apply) {
+    toggleDropdown(e.currentTarget);
+  }
+
+  if (footerButtons.clear) {
+    checkChangeItemOnce(items) ? removeDisabledBtn(footerButtons.clear) : footerButtons.clear.setAttribute('disabled', '');
   }
 }
 
