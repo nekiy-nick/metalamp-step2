92beb6fbb883e5d7cd01148f3442191f62db74b7
Nickolai Khitrov <deliriumbred@yandex.ru>
2022-04-14 08:19:00 +0300

    Import all js files to the main _components.js

---
+++ src/components/_components.js
@@ -0,0 +1,6 @@
+import './_like-button/_like-button';
+import './_rate-button/_rate-button';
+import './_text-field/_text-field';
+import './_dropdown/_dropdown';
+import './_expandable-checkbox-list/_expandable-checkbox-list';
+import './_header/_header';
